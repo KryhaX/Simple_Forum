d1821141cd99d00ceb313fed2dd06ac63d75ff49
KryhaX <krystiwierciak1@interia.pl>
2023-09-05 23:35:58 +0200

    Dodanie pliku glowny_static. Stworzenie struktór dla plików: glowny.html , logowanie.html, rejestracja.html

--- simple_forum/settings.py
+++ simple_forum/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # BootStrap
+    'django_bootstrap5',
+    'bootstrapform',
+
+
 ]
 
 MIDDLEWARE = [
