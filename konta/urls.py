c6890a8f84425201066555560833506054f4880e
KryhaX <krystiwierciak1@interia.pl>
2023-09-08 10:42:31 +0200

    Dodanie views.py przekierowania do moje_konto po zalogowaniu

--- konta/urls.py
+++ konta/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 # Logowanie i Rejestracja
-from konta.views import logowanie
+from konta.views import logowanie, moje_konto
 
 urlpatterns = [
     path('logowanie/', logowanie, name="logowanie"),
+    path('moje_konto/', moje_konto, name="moje_konto"),
 
 ]
